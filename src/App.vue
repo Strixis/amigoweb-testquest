245af2bb6c849823f54ea8b5d324618027f1dac7
Strixis <nivem-strixis@mail.ru>
2021-02-07 14:10:57 +0300

    Подготовлено рабочее место

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img :src="logo" alt="logo">
+    <Test />
+  </div>
+</template>
+
+<script>
+import Test from "components/Test";
+
+import logo from 'img/favicon.png';
+
+export default {
+  components: {
+    Test,
+  },
+
+  data() {
+    return {
+      logo,
+    };
+  },
+}
+</script>
+
+<style lang="sass">
+@use 'styles'
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <span class="test">Test</span>
+  </div>
+</template>
